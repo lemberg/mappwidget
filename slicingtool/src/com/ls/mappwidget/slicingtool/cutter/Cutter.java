ff85e9a0057f6aedcbd071fb30b1f3ff0b0f84c7
Matt Mayer <matt@matthewmayer.co.uk>
2015-02-21 20:59:53 +0800

    set the rendering hints before resizing

--- slicingtool/src/com/ls/mappwidget/slicingtool/cutter/Cutter.java
+++ slicingtool/src/com/ls/mappwidget/slicingtool/cutter/Cutter.java
@@ -172,11 +172,13 @@ public class Cutter
 		BufferedImage resizedImage = new BufferedImage(w, h, type);
 		Graphics2D g = resizedImage.createGraphics();
 
-		g.drawImage(originalImage, 0, 0, w, h, null);
+		
 		
 		g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BICUBIC);
 		g.setRenderingHint(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		g.drawImage(originalImage, 0, 0, w, h, null);
 		
 		g.dispose();
 
