4924305d685d0918b8d54c68c2366ddd3d10b04c
Matt Mayer <matt@matthewmayer.co.uk>
2015-02-21 20:39:47 +0800

    improve quality during resize

--- slicingtool/src/com/ls/mappwidget/slicingtool/cutter/Cutter.java
+++ slicingtool/src/com/ls/mappwidget/slicingtool/cutter/Cutter.java
@@ -17,6 +17,7 @@
 package com.ls.mappwidget.slicingtool.cutter;
 
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -172,6 +173,11 @@ public class Cutter
 		Graphics2D g = resizedImage.createGraphics();
 
 		g.drawImage(originalImage, 0, 0, w, h, null);
+		
+		g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+		g.setRenderingHint(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+		
 		g.dispose();
 
 		return resizedImage;
